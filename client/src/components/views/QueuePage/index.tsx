e798e2cc80ea7f4eec615436071116dd83e55996
M <v4nd3rp03l3ns1s@gmail.com>
2022-10-04 17:39:03 -0500

    feat: queue page module refactored

--- client/src/components/views/QueuePage/index.tsx
+++ client/src/components/views/QueuePage/index.tsx
@@ -1,9 +1,9 @@
-declare module './styles.module.css';
+import styles from './styles.module.css';
 
 export function QueuePage () {
 
   return (
-    <div className="queue-page-container">
+    <div className={styles.queue_page_container}>
       <p>Queue Page Filler Text</p>
     </div>
   )
