a2f2f56460fff581b8f1f53490438705d5db2b0b
M <v4nd3rp03l3ns1s@gmail.com>
2022-10-04 16:35:35 -0500

    feat: landing page components linked

--- client/src/components/views/LandingPage/SignupButton/index.tsx
+++ client/src/components/views/LandingPage/SignupButton/index.tsx
@@ -4,7 +4,7 @@ export function SignupButton () {
 
   return (
     <div className="signup-button-container">
-
+      <p>Signup Button Filler Text</p>
     </div>
   )
 }
--- client/src/components/views/LandingPage/index.tsx
+++ client/src/components/views/LandingPage/index.tsx
@@ -1,10 +1,15 @@
+import { LoginButton } from './LoginButton';
+import { SignupButton } from './SignupButton';
+import { WelcomeMascot } from './WelcomeMascot';
 declare module './styles.module.css';
 
 export function LandingPage () {
 
   return (
     <div className="landing-page-container">
-
+      <WelcomeMascot />
+      <SignupButton />
+      <LoginButton />
     </div>
   )
 }
