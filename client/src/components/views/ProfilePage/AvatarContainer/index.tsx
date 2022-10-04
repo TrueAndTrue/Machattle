8cbc30068165734f67199cc0eaf99ac5a345c31d
M <v4nd3rp03l3ns1s@gmail.com>
2022-10-04 16:07:48 -0500

    feat: profile page components boilerplate

---
+++ client/src/components/views/ProfilePage/AvatarContainer/index.tsx
@@ -0,0 +1,10 @@
+declare module './styles.module.css';
+
+export function AvatarContainer () {
+
+  return (
+    <div className="avatar-container">
+
+    </div>
+  )
+}
---
+++ client/src/components/views/ProfilePage/ProfileUpdateForm/index.tsx
@@ -0,0 +1,10 @@
+declare module './styles.module.css';
+
+export function ProfileUpdateForm () {
+
+  return (
+    <div className="profile-update-form-container">
+
+    </div>
+  )
+}
