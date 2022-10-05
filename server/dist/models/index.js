f42dcf29dfd151ba9bddc8623f1e3add8836279a
Jacob W <jtwalsh7@gmail.com>
2022-10-05 15:37:19 -0400

    Heroku

--- server/dist/models/index.js
+++ server/dist/models/index.js
@@ -1,36 +1,10 @@
 'use strict';
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
-const dotenv_1 = __importDefault(require("dotenv"));
 const sequelize_1 = require("sequelize");
-const url_1 = __importDefault(require("url"));
-const dbConfig_1 = require("../config/dbConfig");
-dotenv_1.default.config({
-    path: '.env'
-});
-const config = dbConfig_1.allConfig['stage'];
 const dbConfig = {
     dialect: 'postgres',
     logging: false
 };
-console.log(config.use_env_variable);
-let sequelize;
+const sequelize = new sequelize_1.Sequelize("d918l45mrulfio", "dsxhyebzxdqdfc", "44e1a24702e6da77052894644edbc15b2b3837dd930082962cb398aef4bae2a6", dbConfig);
 exports.sequelize = sequelize;
-const { DATABASE_URL } = process.env;
-if (DATABASE_URL) {
-    const dbUrl = url_1.default.parse(DATABASE_URL);
-    if (dbUrl.auth && dbUrl.path) {
-        const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
-        const password = dbUrl.auth.substr(dbUrl.auth.indexOf(':') + 1, dbUrl.auth.length);
-        const dbName = dbUrl.path.slice(1);
-        const host = dbUrl.hostname;
-        const { port } = dbUrl;
-        exports.sequelize = sequelize = new sequelize_1.Sequelize(dbName, username, password, dbConfig);
-    }
-}
-else {
-    exports.sequelize = sequelize = new sequelize_1.Sequelize('codewars', 'postgres', 'password', dbConfig);
-}
--- server/models/index.ts
+++ server/models/index.ts
@@ -1,39 +1,14 @@
 'use strict';
 
-import dotenv from 'dotenv'
 import { Dialect, Sequelize, Model } from 'sequelize';
-import url from 'url';
 
-import { allConfig } from '../config/dbConfig';
 import { Question } from './Question'
-
-dotenv.config({
-  path: '.env'
-});
-
-const config = allConfig['stage'];
 
 const dbConfig = {
   dialect: 'postgres' as Dialect,
   logging: false
 };
 
-console.log(config.use_env_variable)
-
-let sequelize :Sequelize;
-const { DATABASE_URL } = process.env;
-if (DATABASE_URL ){
-  const dbUrl = url.parse(DATABASE_URL);
-  if (dbUrl.auth &&dbUrl.path){
-    const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
-    const password = dbUrl.auth.substr(dbUrl.auth.indexOf(':') + 1, dbUrl.auth.length);
-    const dbName = dbUrl.path.slice(1);
-    const host = dbUrl.hostname;
-    const { port } = dbUrl;
-    sequelize = new Sequelize(dbName, username, password, dbConfig);
-  }
-} else {
-  sequelize = new Sequelize('codewars', 'postgres', 'password', dbConfig)
-}
+const sequelize = new Sequelize("d918l45mrulfio", "dsxhyebzxdqdfc", "44e1a24702e6da77052894644edbc15b2b3837dd930082962cb398aef4bae2a6", dbConfig);
 
 export { sequelize } 
