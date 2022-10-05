d29ef8a0c4a72f867c791d8f81b1155a3fff80c9
Jacob W <jtwalsh7@gmail.com>
2022-10-05 13:12:23 -0400

    Heroku

--- server/router/router.ts
+++ server/router/router.ts
@@ -2,6 +2,6 @@ import express, { Express, Request, Response } from 'express'
 
 const router = express.Router();
 
-router.get('/', (res :Response , req :Request) => "Hello Heroku")
+router.get('/', (res :Response , req :Request) => res.send("Hello Heroku"));
 
 export { router };
