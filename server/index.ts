aae100b28ad2e380c5a4d03c753480f59fd0fa4c
Jacob W <jtwalsh7@gmail.com>
2022-10-05 13:13:45 -0400

    Heroku

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,16 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv';
 
-import { router } from './router/router'
-
 dotenv.config({
   path: '.env'
 });
 
+const app: Express = express();
 const PORT = process.env.PORT || 3030;
 
-const app = express();
-
-app.use(express.json())
-app.use(router)
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Heroku');
+});
 
 app.listen(PORT, () => {
     return console.log(`[server]: Server is running on ${PORT}`);
