de4781bd2ac4340c38b81866f94ac10776088ec2
M <v4nd3rp03l3ns1s@gmail.com>
2022-10-07 11:54:56 -0500

    feat: styling for navbar, event feed

--- client/src/components/views/HomePage/RecentEventsFeed/index.tsx
+++ client/src/components/views/HomePage/RecentEventsFeed/index.tsx
@@ -1,11 +1,14 @@
 import { RecentEventsCard } from '../RecentEventsCard/index';
 import styles from './styles.module.css';
 
+//devMocks
+import { mockEvents } from '../homeMocks';
+
 export function RecentEventsFeed () {
 
   return (
     <div className={styles.recent_events_feed_container}>
-      <RecentEventsCard />
+      {mockEvents.map(singleEvent => <RecentEventsCard data={singleEvent} />)}
     </div>
   )
 }
--- client/src/components/views/HomePage/index.tsx
+++ client/src/components/views/HomePage/index.tsx
@@ -13,10 +13,12 @@ export function HomePage () {
 
   return (
     <div className={styles.home_page_container}>
-      <MatchButton />
+      <SelectMatchContainer />
+      <RecentEventsFeed />
+      {/* <MatchButton />
       <h2>Socket IO Information</h2>
       <div>Your user ID: {uid}</div>
-      <div>Users Online: {users.length}</div>
+      <div>Users Online: {users.length}</div> */}
     </div>
   )
 }
