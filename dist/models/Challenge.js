9a945a4e422c1cf4e836da24a3a2796e8a3c46e4
Austin Westbury <austinwestbury@live.com>
2022-10-08 18:53:45 -0400

    fixed syntax

--- dist/models/Challenge.js
+++ dist/models/Challenge.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Challenge = void 0;
-const sequelize_1 = require("sequelize");
-const index_1 = require("./index");
-const Question_1 = require("./Question");
-class Challenge extends sequelize_1.Model {
-}
-exports.Challenge = Challenge;
-Challenge.init({
-    id: {
-        type: sequelize_1.DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    tie: {
-        type: sequelize_1.DataTypes.BOOLEAN
-    },
-    winnerId: {
-        type: sequelize_1.DataTypes.STRING
-    },
-    loserId: {
-        type: sequelize_1.DataTypes.STRING
-    },
-    questionId: {
-        type: sequelize_1.DataTypes.INTEGER
-    }
-}, {
-    tableName: "challenges",
-    sequelize: index_1.sequelize,
-});
-Challenge.belongsTo(Question_1.Question);
-Question_1.Question.hasMany(Challenge, { foreignKey: 'questionId' });
-=======
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Challenge = void 0;
@@ -66,4 +31,3 @@ Challenge.init({
 });
 Challenge.belongsTo(Question_1.Question, { foreignKey: 'questionId' });
 Question_1.Question.hasMany(Challenge, { foreignKey: 'questionId' });
->>>>>>> 31d6236fed495308b79282841743ea4ab213cfb3
--- dist/models/User.js
+++ dist/models/User.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-const sequelize_1 = require("sequelize");
-const index_1 = require("./index");
-const Question_1 = require("./Question");
-class User extends sequelize_1.Model {
-}
-exports.User = User;
-User.init({
-    id: {
-        type: sequelize_1.DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    uid: {
-        type: sequelize_1.DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    username: {
-        type: new sequelize_1.DataTypes.STRING(128),
-        allowNull: false,
-    },
-    rank: {
-        type: new sequelize_1.DataTypes.STRING(128),
-        allowNull: true,
-    },
-    image: {
-        type: new sequelize_1.DataTypes.STRING(128),
-        allowNull: false,
-    },
-    rating: {
-        type: new sequelize_1.DataTypes.INTEGER,
-        allowNull: false,
-    },
-}, {
-    tableName: "users",
-    sequelize: index_1.sequelize,
-});
-User.belongsToMany(User, { as: 'Friends', through: 'UserFriends' });
-User.hasMany(Question_1.Question, {
-    sourceKey: "id",
-    foreignKey: "ownerId",
-    as: "completedQuestions",
-});
-// //two 1-to-many associations between user and challanges
-// User.hasMany(Challenge, {
-//   sourceKey: "uid",
-//   foreignKey: "winnerId",
-//   as: "WonChallanges",
-// });
-// User.hasMany(Challenge, {
-//   sourceKey: "uid",
-//   foreignKey: "loserId",
-//   as: "LostChallanges",
-// });
-// Challenge.belongsToMany(User, {through : 'UserChallenge'});
-// Challenge.belongsTo(User, { foreignKey: "winner"});
-// Challenge.belongsTo(User, { foreignKey: "loser"});
-Question_1.Question.belongsTo(User, { foreignKey: "ownerId" });
-=======
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
@@ -118,4 +55,3 @@ Challenge_1.Challenge.belongsTo(User, { foreignKey: "winnerId" });
 Challenge_1.Challenge.belongsTo(User, { foreignKey: "loserId" });
 User.belongsToMany(Question_1.Question, { through: 'userQuestions' });
 Question_1.Question.belongsToMany(User, { through: 'userQuestions' });
->>>>>>> 31d6236fed495308b79282841743ea4ab213cfb3
