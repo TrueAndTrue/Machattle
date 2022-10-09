815b0c1c6e332bf5ca6a4b50a9e89ed04113f91d
Jacob W <jtwalsh7@gmail.com>
2022-10-09 12:30:15 -0400

    Heroku

--- dist/models/UserFriend.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserFriend = void 0;
-const sequelize_1 = require("sequelize");
-const _1 = require(".");
-class UserFriend extends sequelize_1.Model {
-}
-exports.UserFriend = UserFriend;
-UserFriend.init({}, { tableName: "userFriends", sequelize: _1.sequelize, });
