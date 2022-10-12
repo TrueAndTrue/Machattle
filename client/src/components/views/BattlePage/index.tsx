891275438582462c4cc7329c832cb7f7b7eeb44f
Austin Westbury <austinwestbury@live.com>
2022-10-12 12:00:19 -0400

    battle popup added

--- client/src/components/views/BattlePage/index.tsx
+++ client/src/components/views/BattlePage/index.tsx
@@ -7,6 +7,7 @@ import { CodingArena } from "./CodingArena/index";
 import { InstructionsContainer } from "./InstructionsContainer/index";
 import styles from "./styles.module.css";
 import { getUserById } from "../../../services/userServices";
+import { Popup } from "./Popup/index";
 
 const initialQuestion: IQuestion = {
   id: -1,
@@ -20,8 +21,9 @@ const initialQuestion: IQuestion = {
 export function BattlePage() {
   const dispatch = useDispatch();
   const { player1, player2 } = useSelector((state: any) => state.match);
-  const [thisUsername, setThisUsername] = useState("");
   const thisUser = useSelector((state: any) => state.currentUser.uid);
+  const [thisUsername, setThisUsername] = useState("");
+  const [trigger, setTrigger] = useState(false);
   const [opponentUsername, setOpponentUsername] = useState("");
 
   useEffect(() => {
@@ -45,13 +47,18 @@ export function BattlePage() {
     dispatch(updateQuestion(newQuestion));
   }
 
+  useEffect(() => {
+    console.log('trig!')
+  }, [trigger])
+
   return (
     <div className={styles.battle_page_container}>
+      {trigger && <Popup />}
       <div className={styles.battle_title}>
         <p>{thisUsername} (me)</p> <p> VS </p> <p> {opponentUsername} </p>
       </div>
       <div className={styles.battle_page}>
-        <InstructionsContainer />
+        <InstructionsContainer setTrigger={setTrigger}/>
         <CodingArena />
       </div>
     </div>
