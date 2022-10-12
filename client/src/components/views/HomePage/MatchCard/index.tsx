83caabfcc97a7bea603d383187779859b0ef7dae
M <v4nd3rp03l3ns1s@gmail.com>
2022-10-12 13:51:12 -0500

    feat: added additional animations

--- client/src/components/views/HomePage/MatchCard/index.tsx
+++ client/src/components/views/HomePage/MatchCard/index.tsx
@@ -1,6 +1,7 @@
 import { MatchButton } from "../MatchButton/index";
 import styles from "./styles.module.css";
-import robo from "../../../../assets/CodeBot004.gif";
+import robo1 from "../../../../assets/CodeBot006.gif";
+import robo2 from "../../../../assets/CodeBot005.gif";
 
 interface IProps {
   mode: string;
@@ -10,17 +11,20 @@ export const MatchCard = ({ mode }: IProps) => {
   let matchObj = {
     title: "",
     buttonText: "",
+    source: "",
     body: "",
   };
 
   if (mode === "ranked") {
     matchObj.title = "RANKED BATTLE";
     matchObj.buttonText = "Find Match";
+    matchObj.source = robo1;
     matchObj.body =
       "Complete a challenge head-to-head with another opponent to improve your ranking!";
   } else {
     matchObj.title = "PRACTICE BATTLE";
     matchObj.buttonText = "Enter Match";
+    matchObj.source = robo2;
     matchObj.body =
       "Complete a coding challenge alone. Practice your skills, without changing ranking!";
   }
@@ -32,7 +36,7 @@ export const MatchCard = ({ mode }: IProps) => {
       </div>
       <p className={styles.body_text}>{matchObj.body}</p>
       <MatchButton data={matchObj.buttonText} mode={mode} />
-      <img src={robo} alt="robot logo" className={styles.robot_mascot} />
+      <img src={matchObj.source} alt="robot logo" className={styles.robot_mascot} />
     </div>
   );
 };
