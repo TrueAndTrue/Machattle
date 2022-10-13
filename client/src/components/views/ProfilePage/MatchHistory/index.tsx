be6ee4e4efe95c87fd3001f3045082d1fe9d9180
Jacob W <jtwalsh7@gmail.com>
2022-10-13 16:43:45 -0400

    edited user challenges

--- client/src/components/views/ProfilePage/MatchHistory/index.tsx
+++ client/src/components/views/ProfilePage/MatchHistory/index.tsx
@@ -1,12 +1,24 @@
-import { useContext } from 'react';
+import { useContext, useEffect,useState } from 'react';
 
+import { IChallenge } from '../../../../types';
+import { getUserChallenges } from '../../../../services/userServices';
 import { MatchCard } from '../MatchCard';
 import { UserContext } from '..';
+
 import styles from './styles.module.css';
 
 export function MatchHistory () {
+  const user = useContext(UserContext).user
+  const [challenges, setChallenges] = useState<IChallenge[]>([])
 
-  const challenges = useContext(UserContext).user.challenges || []; 
+  useEffect(()=> {
+    getChallenges()
+  },[])
+
+  const getChallenges = async () => {
+    const userChallenges = await getUserChallenges(user.uid)
+    setChallenges(userChallenges)
+  }
 
   return (
     <div className={styles.match_history_container}>
