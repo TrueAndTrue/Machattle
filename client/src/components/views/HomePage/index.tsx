74fd56e11529ece089a9c17638d910e3fa58e104
Austin Westbury <austinwestbury@live.com>
2022-10-14 12:56:30 -0400

    removelisteners

--- client/src/components/views/HomePage/index.tsx
+++ client/src/components/views/HomePage/index.tsx
@@ -10,6 +10,11 @@ import styles from './styles.module.css';
 export function HomePage () {
 
   const { uid, users } = useContext(SocketContext).SocketState
+  const { socket } = useContext(SocketContext).SocketState;
+  socket?.connect();
+  useEffect(() => {
+    socket?.removeAllListeners();
+  }, [])
 
   return (
     <div className={styles.home_page_container}>
